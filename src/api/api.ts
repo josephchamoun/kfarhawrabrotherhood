3c7428343add52cca06a00b22e7d0e5def2720ef
justincarter100 <chamounjoseph78@gmail.com>
2026-01-23 10:24:29 +0200

    minor changes

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL || "https://brotherhood-backend-1.onrender.com/api";
+
 
 const api = axios.create({
   baseURL: API_URL, // now already includes /api
